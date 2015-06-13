72bcb391936779fde1713f6090720a7ed2c22e45
Josh Lee <Lee45276@gmail.com>
2015-06-13 23:39:06 +0800

    Added draw function, prompt for user input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,16 +1,17 @@
+require"pry"
 # 1. Come up with the requirements/ specifications, which will determine the scope
 # 2. Application logic; sequence of steps
 # 3. Translation of those steps to code
 # 4. Run code to verify logic
 
 # draw a board
-# Assign player1 to "x" 
-# Assign player2 to "O"
+# Assign player to "x" 
+# Assign computer to "O"
 
 # loop until a winner or all swuares are taken
   # player1 picks an empty square
   # check for a winner
-  # player2 picks an empty square 
+  # computer picks an empty square 
   # check for a winner
 
 # if there's a winner
@@ -18,9 +19,34 @@
   # or else
    # It's a tie
 
-
-def draw_board
-  puts " | | "
-  puts " -+- "
-  puts " | | "
+def initialize_board
+  b = {}
+  (1..9).each{ |position| b[position] = " "}
+  b
 end
+
+def draw_board(b)
+  puts "#{b[1]}  |  #{b[2]}  |  #{b[3]} "
+  puts " -------------- "
+  puts "#{b[4]}  |  #{b[5]}  |  #{b[6]} "
+  puts " -------------- "
+  puts "#{b[7]}  |  #{b[8]}  |  #{b[9]} "
+end
+
+def player_picks_square(b)
+  puts "Pick a square (1-9)"
+  position = gets.chomp.to_i
+  b[position] = "X"
+end
+
+board = initialize_board
+
+draw_board(board)
+     player_picks_square(board)
+
+# begin
+
+# end until # winner || all_squares_taken?
+
+# start at 38:04
+
