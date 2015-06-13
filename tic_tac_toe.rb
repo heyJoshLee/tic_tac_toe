28937dca9a0888d15cac4b5d6cf8a285df9104df
Josh Lee <Lee45276@gmail.com>
2015-06-13 20:41:33 +0800

    initial commit

---
+++ tic_tac_toe.rb
@@ -0,0 +1,26 @@
+# 1. Come up with the requirements/ specifications, which will determine the scope
+# 2. Application logic; sequence of steps
+# 3. Translation of those steps to code
+# 4. Run code to verify logic
+
+# draw a board
+# Assign player1 to "x" 
+# Assign player2 to "O"
+
+# loop until a winner or all swuares are taken
+  # player1 picks an empty square
+  # check for a winner
+  # player2 picks an empty square 
+  # check for a winner
+
+# if there's a winner
+  #   show the winner
+  # or else
+   # It's a tie
+
+
+def draw_board
+  puts " | | "
+  puts " -+- "
+  puts " | | "
+end
