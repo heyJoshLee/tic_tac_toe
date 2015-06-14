18151929119b1ad14b680bba4ba2a1003680f375
Josh Lee <Lee45276@gmail.com>
2015-06-14 19:38:42 +0800

    added ability to choose a space and win condition. Need to fix bugs, available spaces, computer not winning, computer chosing random spaces

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -26,11 +26,16 @@ def initialize_board
 end
 
 def draw_board(b)
+  system "clear"
   puts "#{b[1]}  |  #{b[2]}  |  #{b[3]} "
   puts " -------------- "
   puts "#{b[4]}  |  #{b[5]}  |  #{b[6]} "
   puts " -------------- "
   puts "#{b[7]}  |  #{b[8]}  |  #{b[9]} "
+end
+
+def empty_positions(b)
+  b.select {|k, v| v == " "}.keys
 end
 
 def player_picks_square(b)
@@ -39,14 +44,44 @@ def player_picks_square(b)
   b[position] = "X"
 end
 
+def computer_picks_square(b)
+  position = empty_positions(b).sample
+  b[position] = "O"
+end
+
+def check_winner(b)
+  winning_lines =[[1,2,3], [4,5,6], [7,8,9], [1,4,7],
+                  [2,5,8], [3,6,9], [1,5,9], [3,5,7]
+                ]
+  winning_lines.each do |line|
+    if b[line[0]] == "X" and b[line[1]] == "X" and b[line[2]] == "X"
+      return "Player"
+    elsif b[line[0]] == "O" and b[line[1]] == "O" and b[line[2]] == "O"
+      return "Computer"
+    else
+      return nil
+    end
+  end
+end
+
+
 board = initialize_board
-
 draw_board(board)
-     player_picks_square(board)
+     
 
-# begin
+begin
+  player_picks_square(board)
+  computer_picks_square(board)
+  draw_board(board)
+  winner = check_winner(board)
 
-# end until # winner || all_squares_taken?
 
-# start at 38:04
+end until winner || empty_positions(board).empty?
+
+  if winner
+    puts "#{winner} won!"
+  else
+    puts "It's a tie!"
+  end
+
 
