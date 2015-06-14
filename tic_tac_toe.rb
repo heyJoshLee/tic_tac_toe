80ce80c3ed02bb657f7b127253e149b66021b88f
Josh Lee <Lee45276@gmail.com>
2015-06-14 20:02:32 +0800

    Fixed issue so computer can now win, added move validation

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,19 +1,21 @@
 require"pry"
+
 # 1. Come up with the requirements/ specifications, which will determine the scope
 # 2. Application logic; sequence of steps
 # 3. Translation of those steps to code
 # 4. Run code to verify logic
-
+#
 # draw a board
+
 # Assign player to "x" 
 # Assign computer to "O"
-
+#
 # loop until a winner or all swuares are taken
   # player1 picks an empty square
   # check for a winner
   # computer picks an empty square 
   # check for a winner
-
+#
 # if there's a winner
   #   show the winner
   # or else
@@ -39,8 +41,10 @@ def empty_positions(b)
 end
 
 def player_picks_square(b)
-  puts "Pick a square (1-9)"
-  position = gets.chomp.to_i
+  begin
+    puts "Pick a square (1-9)"
+    position = gets.chomp.to_i
+  end until empty_positions(b).include?(position)
   b[position] = "X"
 end
 
@@ -50,36 +54,34 @@ def computer_picks_square(b)
 end
 
 def check_winner(b)
-  winning_lines =[[1,2,3], [4,5,6], [7,8,9], [1,4,7],
-                  [2,5,8], [3,6,9], [1,5,9], [3,5,7]
-                ]
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
   winning_lines.each do |line|
-    if b[line[0]] == "X" and b[line[1]] == "X" and b[line[2]] == "X"
-      return "Player"
-    elsif b[line[0]] == "O" and b[line[1]] == "O" and b[line[2]] == "O"
-      return "Computer"
-    else
-      return nil
-    end
+    return "Player" if b.values_at(*line).count('X') == 3
+    return "Computer" if b.values_at(*line).count('O') == 3
   end
+  nil
 end
 
+def nine_positions_are_filled?(board)
+  empty_positions(board) == []
+end
+
+def announce_winner(winner)
+  puts "#{winner} wins!"
+end
 
 board = initialize_board
 draw_board(board)
      
-
 begin
   player_picks_square(board)
   computer_picks_square(board)
   draw_board(board)
   winner = check_winner(board)
-
-
-end until winner || empty_positions(board).empty?
+end until winner || nine_positions_are_filled?(board)
 
   if winner
-    puts "#{winner} won!"
+    announce_winner(winner)
   else
     puts "It's a tie!"
   end
